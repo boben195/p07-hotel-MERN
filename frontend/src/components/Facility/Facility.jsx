393594951860d053522fa2b8403198b8b939c3a7
boben195 <boben195@ukr.net>
2025-07-21 17:29:35 +0300

    add facility

--- frontend/src/components/Facility/Facility.jsx
+++ frontend/src/components/Facility/Facility.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import {
+  FaBath,
+  FaCar,
+  FaCocktail,
+  FaConciergeBell,
+  FaShuttleVan,
+  FaSwimmingPool,
+} from "react-icons/fa";
+
+const services = [
+  {
+    icon: <FaShuttleVan size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+  {
+    icon: <FaConciergeBell size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+  {
+    icon: <FaSwimmingPool size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+  {
+    icon: <FaBath size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+  {
+    icon: <FaCar size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+  {
+    icon: <FaCocktail size={32} />,
+    title: "Pick and Drop",
+    desc: "We’ll pick up from airort while you comfy on your ride",
+  },
+];
 
 const Facility = () => {
-  return <div>Facility</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <p>Servises</p>
+          <h2>Facilities & Services</h2>
+        </div>
+        <div>
+          {services.map((service, index) => (
+            <div key={index}>
+              <div>{service.icon}</div>
+              <h3>{service.title}</h3>
+              <p>{service.desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Facility;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "../../components/Hero/Hero";
 import HotelList from "../../components/HotelList/HotelList";
-
+import Facility from "../../components/Facility/Facility";
 const Home = () => {
   return (
     <div>
       <Hero />
       <HotelList />
+      <Facility />
     </div>
   );
 };
