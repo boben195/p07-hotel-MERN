2d5ae32e533f8e8236a49f526307035f6e8cd33e
boben195 <boben195@ukr.net>
2025-07-22 10:13:57 +0300

    add footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <h2>Sign Up For Exclusive Offers</h2>
+
+        <div>
+          <input type="email" placeholder="Enter your email" />
+          <button>Join Now</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h2>DELUXE HOTELS</h2>
+          <div>
+            <FaFacebook />
+            <FaInstagram />
+            <FaYoutube />
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>HOME</li>
+            <li>BOOKING</li>
+            <li>ROOMS</li>
+            <li>CONTACT</li>
+          </ul>
+        </div>
+      </div>
+      <p>Copyright and bla bla bla</p>
+    </div>
+  );
 };
 
 export default Footer;
