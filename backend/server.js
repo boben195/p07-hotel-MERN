71f2dd7ccecff85ef0ca8dbe1c1b5c1fed365586
boben195 <boben195@ukr.net>
2025-08-07 15:56:37 +0300

    init server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+});
+
+app.listen(port, () => console.log("Server started on port " + port)
+)
