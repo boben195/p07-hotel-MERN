6474aa78b25212a148a90273c182faa4d3a7fe9d
boben195 <boben195@ukr.net>
2025-08-08 12:25:39 +0300

    add addHotel func

--- backend/controllers/hotelControllers.js
+++ backend/controllers/hotelControllers.js
@@ -4,8 +4,33 @@ import { v2 as cloudinary } from "cloudinary";
 
 
 const addHotel = async (req, res) => {
-    res.json({success: true, message: "Add workingg"})
+    try {
+        const { name, price, description } = req.body;
+        const image = req.file;
+        let imageUrl = "";
 
+
+        if (image) {
+            let result = await cloudinary.uploader.upload(image.path, { resource_type: "image" });
+            imageUrl = result.secure_url
+        } else {
+            imageUrl = "https://via.placeholde.com/150"
+        }
+
+        const hotelData = {
+            name, description, price: Number(price),
+            image: imageUrl,
+            date: Date.now()
+        }
+
+        const hotel = new hotelModel(hotelData);
+        await hotel.save()
+
+        res.json({success: true, message: "Hotel room added successfully"})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: "Hotel room NOT added successfully! ERROR"})
+    }
 }
 
 const listHotel = async (req, res) => {
