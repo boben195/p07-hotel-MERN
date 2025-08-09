6ec054a883411a4da544312cc7e6c60a7ac2c74c
boben195 <boben195@ukr.net>
2025-08-09 20:25:14 +0300

    small mistake

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -4,7 +4,7 @@ const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        if (email === process.env.ADMIN_EMAIL && password === password.env.ADMIN_PASSWORD) {
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
             const token = jwt.sign(email + password, process.env.JWT_SECRET)
             res.json({ success: true, token })
             
