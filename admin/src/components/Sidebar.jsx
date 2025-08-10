a0423aa8b6a1ccc275ad8c06dd2c6088e8997cf3
boben195 <boben195@ukr.net>
2025-08-10 10:48:04 +0300

    add sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { IoIosLogOut, IoMdAddCircleOutline } from "react-icons/io";
+import { MdChecklistRtl, MdFormatListBulletedAdd } from "react-icons/md";
 
 const Sidebar = ({ setToken }) => {
-  return <div>Sidebar</div>;
+  return (
+    <div>
+      <div>
+        <h2>Delux Hotel</h2>
+      </div>
+      <div>
+        <NavLink to="/add">
+          <IoMdAddCircleOutline />
+          <p>Add rooms</p>
+        </NavLink>
+        <NavLink to="/list">
+          <MdFormatListBulletedAdd />
+          <p>Rooms List</p>
+        </NavLink>
+        <NavLink to="/reservation">
+          <MdChecklistRtl />
+          <p>Reservations</p>
+        </NavLink>
+
+        <button>
+          <IoIosLogOut />
+          <p>LogOut</p>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
