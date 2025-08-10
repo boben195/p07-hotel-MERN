0ddfb87e61b2365aaae945d060cfe43efefd8fdd
boben195 <boben195@ukr.net>
2025-08-10 10:15:51 +0300

    add admin login

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { backendUrl } from "../App";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Admin Login</h1>
+          <form>
+            <div>
+              <p>Email Address</p>
+              <input
+                type="email"
+                placeholder="Enter admin email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <p>Password</p>
+              <input
+                type="password"
+                placeholder="Enter admin password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
