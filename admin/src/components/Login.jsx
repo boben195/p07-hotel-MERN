f2ebdaa9f435c510b86f9efd49400c9ac412a10c
boben195 <boben195@ukr.net>
2025-08-10 10:24:42 +0300

    add style for login

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -7,29 +7,44 @@ const Login = () => {
 
   return (
     <div>
-      <div>
-        <div>
-          <h1>Admin Login</h1>
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="bg-white shadow-md rounded-lg px-8 py-6 w-full max-w-md">
+          <h1 className="text-2xl font-bold text-center text-gray-800 mb-4">
+            Admin Login
+          </h1>
           <form>
-            <div>
-              <p>Email Address</p>
+            <div className="mb-4">
+              <p className="text-sm font-semibold text-gray-600 mb-2">
+                Email Address
+              </p>
               <input
                 type="email"
                 placeholder="Enter admin email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
+                required
+                className="w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-300"
               />
             </div>
-            <div>
-              <p>Password</p>
+            <div className="mb-4">
+              <p className="text-sm font-semibold text-gray-600 mb-2">
+                Password
+              </p>
               <input
                 type="password"
                 placeholder="Enter admin password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-[95%] px-3 py-2 border border-gray-300 rounded-md text-sm focus:outline-none focus:border-b-gray-300"
               />
             </div>
-            <button>Login</button>
+            <button
+              type="submit"
+              className="w-full px-3 py-2 text-lg font-bold bg-fuchsia-900 rounded-md text-white"
+            >
+              Login
+            </button>
           </form>
         </div>
       </div>
