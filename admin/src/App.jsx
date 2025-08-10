6f78e491513a4662502007f4bee85f955b4163b7
boben195 <boben195@ukr.net>
2025-08-10 10:09:00 +0300

    add routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import Login from "./components/Login";
+import Sidebar from "./components/Sidebar";
+import AddHotel from "./pages/AddHotel";
+import ListHotel from "./pages/ListHotel";
+import Reservation from "./pages/Reservation";
+
+export const backendUrl = "http://localhost:4000";
 
 const App = () => {
-  return <div className="text-red-600">App</div>;
+  const [token, setToken] = useState("");
+  return (
+    <div>
+      {!token ? (
+        <Login />
+      ) : (
+        <>
+          <div>
+            <Sidebar />
+            <div>
+              <Routes>
+                <Route path="/add" element={<AddHotel />} />
+                <Route path="/list" element={<ListHotel />} />
+                <Route path="/reservation" element={<Reservation />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
